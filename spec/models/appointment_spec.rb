6cf39e990e65be992a57b09be754c7e8c06610ef
Ale Paredes <aept7714@gmail.com>
2014-04-13 16:51:16 -0430

    Adds specs of an Appointment

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe Appointment do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+    describe 'validations' do
+        before do
+            @appointment = Fabricate(:appointment, start_time: DateTime.now + 1.minute, 
+                end_time: DateTime.now  + 1.hour)
+        end
+
+        it { expect(@appointment).to validate_presence_of(:first_name) }
+        it { expect(@appointment).to validate_presence_of(:last_name) }
+        it { expect(@appointment).to validate_presence_of(:start_time) }
+        it { expect(@appointment).to validate_presence_of(:end_time) }
+    end
+    
+    describe 'db table' do
+        before do
+            @appointment = Fabricate(:appointment)
+        end
+
+        it { expect(@appointment).to have_db_index(:id) }
+        it { expect(@appointment).to have_db_column(:first_name).
+                of_type(:string).with_options(null: false)}
+        it { expect(@appointment).to have_db_column(:last_name).
+                of_type(:string).with_options(null: false)}
+        it { expect(@appointment).to have_db_column(:start_time).
+                of_type(:datetime).with_options(null: false)}
+        it { expect(@appointment).to have_db_column(:end_time).
+                of_type(:datetime).with_options(null: false)}
+        it { expect(@appointment).to have_db_column(:comment).
+                of_type(:string).with_options(null: true)}
+    end
+
 end
