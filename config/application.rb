aa389e8c928b4a55dfc0ae53e8446f25df480125
Ale Paredes <aept7714@gmail.com>
2014-04-13 16:42:20 -0430

    Congif of Fabricator gem for test fixtures

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,13 @@ module AppointmentApi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.i18n.enforce_available_locales = false
+    
+    config.generators do |g|
+      g.test_framework      :rspec, fixture: true
+      g.fixture_replacement :fabrication
+    end
+    
   end
 end
